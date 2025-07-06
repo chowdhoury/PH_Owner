1643c837a753a31dba72ee82b5129f34055e880b
Raihan <mdr8077@gmail.com>
2025-07-07 00:49:31 +0600

    some done

---
+++ Exam/practice.js
@@ -0,0 +1,21 @@
+
+        let x=0;
+        function getText(){
+            document.getElementById('demo').innerHTML='Raihan';
+        }
+        function getSize(){
+            document.getElementById('demo').style.fontSize='35px';
+        }
+        function getDisNone(){
+            document.getElementById('demo').style.display='none';
+        }
+        function getSum(){
+            document.getElementById('demo').style.display='block';
+            document.getElementById('demo').innerHTML=x+=1;
+        }
+        function getAlert(){
+            window.alert("Welcome");
+        }
+        function getLog(){
+            console.log("Working");
+        }
